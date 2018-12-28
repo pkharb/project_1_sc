ea0e4a8aca1cc09944b5018e8952ba288e0b68e0
pk <pradeepkharb@hotmail.com>
2018-12-28 14:18:53 -0800

    added package.json, installed express, ,morgan, dotenv and mongoose, connected to port 3000

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const
+    express = require('express'),
+    app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World!!");
+});
+
+
+app.listen(3000);
