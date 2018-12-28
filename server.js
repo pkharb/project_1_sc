ac8bdb8107b5c76547e6bfe0a776804b591550de
pk <pradeepkharb@hotmail.com>
2018-12-28 14:51:47 -0800

    created databse on mlab and made a connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const
     express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.port,
+    mongoose = require('mongoose'),
+    logger = require('morgan');
 
+// database connection
+mongoose.connect('mongodb://admin:admin1@ds145304.mlab.com:45304/project_1_sc');
+
+// routes
 app.get('/', (req, res) => {
-    res.send("Hello World!!");
+    res.send("Hello World Yayyy!!");
 });
 
 
-app.listen(3000);
+app.listen(port, err => {
+    console.log(err || `Server connected to port: ${port}`);
+});
